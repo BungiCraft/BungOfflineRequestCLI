9b3767eb1e41463bcebbb96b3cc50bd363638175
BungiCraft <dagivans@gmail.com>
2025-03-26 05:15:55 +0100

    Improved tool and updated README.md

--- README.md
+++ README.md
@@ -1,11 +1,43 @@
-## Meow :3c (aka installation)
-* U can do this in cmd and powershell!
-* ![IMG](pics/cli.png)
-* Just add path to the exe in ur enviroment variables like this! (the file can be placed anywhere, u choose :3)
-* ![IMG](pics/variable.png)
+# BungOfflineRequestCLI
 
-## Requires
-* This [Plugin](https://github.com/BungiCraft/BungOfflineRequest)
+A simple CLI tool for the BungOfflineRequest plugin.
+
+Enables you to do this!
+
+![IMG](pics/cli.png)
+
+## Features
+
+- Request BSR codes to yourself, that later are used by the [BungOfflineRequest](https://github.com/BungiCraft/BungOfflineRequest) plugin to show up in game.
+- Supports multiple codes at once!
+- Easy to use, just type `bsr <bsr-code>` in your terminal.
+- No need to open the game to request a BSR code.
+- Also filters out invalid codes, so no need to worry about that.
+- That means you can just copy and paste the codes from beatsaver and it will work! (even using the "Copy BSR" button on beatsaver that includes the "!bsr" command, it'll get filtered :3).
+
+## Usage
+
+- `bsr <bsr-code>` - Request a BSR code to yourself like this.
+- `bsr <bsr-code1> <bsr-code2> <bsr-code3> ` - Supports multiple codes at once, just separate them with a space!
+
+## Installation
+
+Just put the exe file anywhere you want, and add the path to your environment variables.
+
+Also needs the [BungOfflineRequest](https://github.com/BungiCraft/BungOfflineRequest) plugin to be useful, without it, it's just a tool that does nothing KEKW.
+
+If u don't want to add it to your environment variables u can still use this tool, but then you wouldn't be able to just write "bsr" into cmd or powershell without not first pointing it to the location of the exe.
+
+### How to add environment variables (Windows)
+
+1. Open the start menu and search for "Environment Variables".
+2. Click on "Edit environment variables for your account".
+3. Find the "Path" variable in the list and click on "Edit".
+4. Click on "New" and paste the path to the folder where the exe is located.
+5. Click "OK" on all the windows to save the changes.
+6. Open a new terminal and type `bsr <bsr-code>` to see if it works.
+7. Done!
 
 ## Author
-* BungiCraft - Meow :3c
+
+BungiCraft - Meow :3c
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::fs::File;
 use std::io::Write;
 use std::path::Path;
 use serde::{Serialize, Deserialize};
+use regex::Regex;
 
 #[derive(Serialize, Deserialize)]
 struct Requests {
@@ -15,30 +16,62 @@ struct Map {
 }
 
 fn main() {
-    let path = env::var("LOCALAPPDATA").expect("No APP_DATA directory");
-    let directory_binding = Path::new("..").join(&path).join(".BungRequest");
-    let directory_path = directory_binding.as_path();
+    // Set up path
+    let path = env::var("LOCALAPPDATA")
+        .expect("No APP_DATA directory");
+    let directory_binding = Path::new("..")
+        .join(&path)
+        .join(".BungRequest");
+    let directory_path = directory_binding
+        .as_path();
+
+    // If the directory doesn't exist, create it
     if !Path::exists(directory_path) {
-        fs::create_dir(directory_path).expect("Error creating directory .BungRequest");
+        fs::create_dir(directory_path)
+            .expect("Error creating directory .BungRequest");
     }
-    let requests_binding = Path::new("..").join(&directory_path).join("requests.json");
-    let requests_path = requests_binding.as_path();
+
+    // Add file to path
+    let requests_binding = Path::new("..")
+        .join(&directory_path)
+        .join("requests.json");
+    let requests_path = requests_binding
+        .as_path();
+
+    // If the file doesn't exist, create it
     if !Path::exists(requests_path) {
-        let mut file = File::create(requests_path).expect("Error creating file requests.json");
-        file.write_all(b"{\r\n    \"maps\": [\r\n    ]\r\n}").expect("Error writing data to file requests.json");
+        let mut file = File::create(requests_path)
+            .expect("Error creating file requests.json");
+        file.write_all(b"{\r\n    \"maps\": [\r\n    ]\r\n}")
+            .expect("Error writing data to file requests.json");
     }
 
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
-        let bsr = &args[1];
-        let mut requests: Requests = serde_json::from_str(&*fs::read_to_string(requests_path).expect("Error reading file requests.json")).unwrap();
-        let request = Map { bsr: String::from(bsr) };
-        if requests.maps.contains(&request) { // Prevent adding the same code twice
-            return println!("Map is already requested!");
+        // Read the requests from the file
+        let mut requests: Requests = serde_json::from_str(&*fs::read_to_string(requests_path)
+            .expect("Error reading file requests.json"))
+            .expect("Error parsing JSON");
+        // Create a regex to check if the argument is a valid BSR code (hexadecimal)
+        let reg = Regex::new(r"^[0-9a-fA-F]+$")
+            .expect("Error creating regex");
+
+        for bsr in args.iter() {
+            if reg.is_match(bsr) {
+                let request = Map { bsr: String::from(bsr) };
+                if requests.maps.contains(&request) { // Prevent adding the same code twice
+                    println!("Map is already requested!");
+                } else {
+                    requests.maps.push(request);
+                    println!("Requested map: {} :3c", bsr);
+                }
+            }
         }
-        requests.maps.push(request);
-        let json = serde_json::to_string(&requests).unwrap();
-        fs::write(requests_path, json).expect("Error writing to file requests.json");
-        println!("Requested map: {} :3c", bsr)
+
+        // Write the updated requests to the file
+        let json = serde_json::to_string(&requests)
+            .expect("Error serializing JSON");
+        fs::write(requests_path, json)
+            .expect("Error writing to file requests.json");
     }
 }
